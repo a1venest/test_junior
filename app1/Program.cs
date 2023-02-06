ae150a10d1a1f05eb3929807467a48312ee51bf4
Aivenest <aivenest@gmail.com>
2023-02-06 22:45:15 +0500

    app1 compleated

---
+++ app1/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] GetString()
+{
+    Console.WriteLine("Введите значения через пробел, по окончании ввода, нажмите Enter: ");
+    return Console.ReadLine().Split(" ");
+}
+void NewInput(string[] arr)
+{
+    string newInput = "";
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        newInput = newInput + arr[i] + ", ";
+    }
+    string lastPosition = arr[arr.Length - 1];
+    Console.WriteLine(newInput + lastPosition + ".");
+}
+
+string[] input = GetString();
+NewInput(input);
