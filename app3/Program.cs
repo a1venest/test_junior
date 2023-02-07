19d801bba6b014017d7d11b3aec5b60e2c348287
Aivenest <aivenest@gmail.com>
2023-02-07 19:49:51 +0500

    app3

---
+++ app3/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetNumberFromConsole()
+{
+    Console.WriteLine("Введите целое число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Case(int a)
+{
+    int temp = a % 10;
+    if (temp == 1) Console.WriteLine(a + " компьютер.");
+    if (temp == 2 || temp == 3 || temp == 4) Console.WriteLine(a + " компьютера.");
+    if (temp == 0 || temp == 5 || temp == 6 || temp == 7 || temp == 8 || temp == 9) Console.WriteLine(a + " компьютеров.");
+}
+int number = GetNumberFromConsole();
+Case(number);
