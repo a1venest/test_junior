d92bc1fca0f72a7a3238e10b5b80c1385ef9c0ac
Aivenest <aivenest@gmail.com>
2023-02-07 19:58:36 +0500

    app4

---
+++ app4/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumberFromConsole()
+{
+    Console.WriteLine("Введите целое число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool PrimeNumber(int a)
+{
+    for (int i = 2; i < a; i++)
+    {
+        if (a % i == 0) return false;
+    }
+    return true;
+}
+int number = GetNumberFromConsole();
+Console.WriteLine(PrimeNumber(number));
+// 7 минут
